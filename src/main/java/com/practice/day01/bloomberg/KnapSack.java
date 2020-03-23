c3fe997729ec37a52b8be71b6f16cbf7e4dd8c94
KV <indianakv@gmail.com>
2020-03-22 21:23:43 -0400

    Sort Practice1

--- src/main/java/com/practice/day01/bloomberg/KnapSack.java
+++ src/main/java/com/practice/day01/bloomberg/KnapSack.java
@@ -19,9 +19,27 @@ public class KnapSack {
     }
 
     public static void main(String[] args) {
-        int val[] = new int[]{60, 100, 120};
-        int wt[] = new int[]{10, 20, 30};
-        int W = 50;
+        //int val[] = new int[]{60, 100, 120};
+        //int wt[] = new int[]{10, 20, 30};
+
+        // n = size of input 
+        // W = target weight  
+        // Absolute space/ time 
+        // Space : 
+        //      Input = 2n + 1 
+        //      Additional = n * W 
+        // Time = n * W   
+        // Big O notation Space / time 
+        // Space 
+        //      Input = O(N) 
+        //      Additional = O(N^2) 
+        //      Overall = O(N^2)  
+        // Time = O(N^2)          
+        // Complexity : O(K), O(LOG(N)), O(N), O(NLOG(N)), O(N^2), O(N^3)
+
+        int wt[] = {1, 56, 42, 78, 12};
+        int val[] = {50, 30, 20, 10, 50};
+        int W = 150;
         System.out.println(compute(wt, val, W, wt.length));
     }
 }
