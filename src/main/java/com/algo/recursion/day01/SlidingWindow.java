14302d565d36d02ff08c0c051222eac1bfebf707
KV <indianakv@gmail.com>
2021-10-09 18:24:44 -0400

    Sliding window Algorithm

---
+++ src/main/java/com/algo/recursion/day01/SlidingWindow.java
@@ -0,0 +1,39 @@
+package com.algo.recursion.day01;
+
+import java.util.Arrays;
+
+import static java.lang.Math.max;
+
+public class SlidingWindow {
+
+    static int[] arr = {1, 9, -1, -2, 7, 3, -1, 2};
+
+    //o(n*k)
+    private static void bruteForce(int k) {
+        int maxWindow = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length - k; i++) {
+            int currentWindow = 0;
+            for (int j = i; j < i + k; j++) {
+                currentWindow += arr[j];
+            }
+            maxWindow = max(currentWindow, maxWindow);
+        }
+        System.out.println(maxWindow);
+    }
+
+    //o(n)
+    private static void algo2(int k) {
+        int winSum = Arrays.stream(arr).limit(k).sum();
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = k; i < arr.length; i++) {
+            winSum += arr[i] - arr[i - k];
+            maxSum = max(winSum, maxSum);
+        }
+        System.out.println(maxSum);
+    }
+
+    public static void main(String[] args) {
+        bruteForce(4);
+        algo2(4);
+    }
+}
--- src/main/java/com/kv/concurrency/sorting/PersonImpl.java
+++ src/main/java/com/kv/concurrency/sorting/PersonImpl.java
@@ -1,6 +1,8 @@
 package com.kv.concurrency.sorting;
 
 import com.kv.concurrency.dto.Person;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.request.GetRequest;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,11 +17,20 @@ public class PersonImpl {
                 new Person("Anusha",24,-1)}));
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         List<Person> people = add();
         System.out.println(people);
         people.sort((p1, p2) -> p1.getRanking() - p2.getRanking());
         System.out.println(people);
 
+       /* HttpResponse<String> response = Unirest.get("https://alpha-vantage.p.rapidapi.com/query?function=GLOBAL_QUOTE&symbol=AAPL&datatype=json")
+                .header("x-rapidapi-key", "f94a94563bmshef4a6c8b12dfa5fp19b6a1jsn69b2b177ed58")
+                .header("x-rapidapi-host", "alpha-vantage.p.rapidapi.com")
+                .asString();
+        System.out.println(response.getBody());*/
+
+
+        GetRequest getRequest = Unirest.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=JPM&adjusted=true&outputsize=compact&apikey=M5YJO28B6QLN8EYF");
+        System.out.println(getRequest.asJson().getBody());
     }
 }
